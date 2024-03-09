fdcb79c801735695303561c1497c3d47adc33da5
Jtolen543 <Jtolen543@gmail.com>
2024-03-09 11:49:23 -0500

    Update files

--- jwt_HW3B.py
+++ jwt_HW3B.py
@@ -7,10 +7,12 @@ def to_hex_string(data):
             hex_string_list.append(chr(i + 55).lower())
     return "".join(hex_string_list)
 
+
 def count_runs(flat_data):
+    global consecutive
     count = 0
-    for i in range(len(flat_data)-1):
-        if flat_data[i] == flat_data[i+1]:
+    for i in range(len(flat_data) - 1):
+        if flat_data[i] == flat_data[i + 1]:
             count += 1
             if count == 4:
                 consecutive = True
@@ -20,7 +22,7 @@ def count_runs(flat_data):
         consecutive = False
     total_runs = 0
     if consecutive == True:
-        for i in range(0,16):
+        for i in range(0, 16):
             runs = flat_data.count(i)
             if i in flat_data:
                 if runs % 15 == 0:
@@ -32,12 +34,13 @@ def count_runs(flat_data):
             total_runs += 1
     return total_runs
 
+
 def encode_rle(flat_data):
     count = 0
     repeat_list = []
     compressed_list = []
-    for i in range(len(flat_data)-1):
-        if flat_data[i] == flat_data[i+1]:
+    for i in range(len(flat_data) - 1):
+        if flat_data[i] == flat_data[i + 1]:
             count += 1
             if count == 4:
                 consecutive = True
@@ -45,42 +48,45 @@ def encode_rle(flat_data):
         else:
             count = 0
         consecutive = False
-    if consecutive == True:
+    if consecutive is True:
         for i in flat_data:
             if i not in repeat_list:
                 repeat_list.append(i)
                 x = flat_data.count(i)
                 if x < 15:
-                    compressed_list.extend([flat_data.count(i),i])
+                    compressed_list.extend([flat_data.count(i), i])
                 elif x > 15:
                     while x > 15:
-                        compressed_list.extend([15,i])
+                        compressed_list.extend([15, i])
                         x -= 15
-                    compressed_list.extend([x,i])
-    elif consecutive == False:
+                    compressed_list.extend([x, i])
+    elif consecutive is False:
         for i in flat_data:
             compressed_list.append(1)
             compressed_list.append(i)
     return compressed_list
 
+
 def get_decoded_length(rle_data):
     sum = 0
-    for i in range (0, len(rle_data),2):
+    for i in range(0, len(rle_data), 2):
         sum += int(rle_data[i])
     return sum
 
+
 def decode_rle(rle_data):
     decode_list = []
-    for i in range(0,len(rle_data),2):
-        for j in range(0,rle_data[i]):
-            decode_list.append(rle_data[i+1])
+    for i in range(0, len(rle_data), 2):
+        for j in range(0, rle_data[i]):
+            decode_list.append(rle_data[i + 1])
     return decode_list
+
 
 def string_to_data(data_string):
     data_string_list = []
     for i in data_string:
         if i in [str(j) for j in range(10)]:
             data_string_list.append(int(i))
-        elif i in ['a','b','c','d','e','f']:
-            data_string_list.append(ord(i)-87)
+        elif i in ['a', 'b', 'c', 'd', 'e', 'f']:
+            data_string_list.append(ord(i) - 87)
     return data_string_list
